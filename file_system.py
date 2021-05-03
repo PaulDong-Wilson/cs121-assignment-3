21ad667070498997f0f5780cdbcb7fafdd55fbc7
PaulDong-Wilson <pdw27276@gmail.com>
2021-05-02 21:55:22 -0700

    Update file_system.py

--- file_system.py
+++ file_system.py
@@ -1,10 +1,8 @@
 
 # File structure
 #       -- each line is a token entry
-# key; number_of_pages; doc_url, term_frequency; doc_url, term_frequency
-# key, [doc_url, term_frequency]
-# key, [doc_url, term_frequency]
-def addPage_index_file(key, url, freq):
+# key; doc_url, term_frequency; doc_url, term_frequency
+def addPage_index_file(keys, urls, freqs):
     filename = ""
     indexInfo = ""
     totalPageNum = 0
@@ -19,7 +17,7 @@ def addPage_index_file(key, url, freq):
     indexInfo.close()
 
     # finds what file the key would be in
-    x = key[0].lower()
+    x = keys[0][0].lower()
     if x == '0' or x == '1' or x == '2' or x == '3' or x == '4' or x == '5' or x == '6' or x == '7' or x == '8' or x == '9':
         filename = "index_0-9.txt"
     elif x == 'a' or x == 'b' or x == 'c' or x == 'd' or x == 'e' or x == 'f':
@@ -31,51 +29,49 @@ def addPage_index_file(key, url, freq):
     else:
         filename = "index_t-z.txt"
     file = ""
+    # load in file into dic
+    dic = {}
     try :
         file = open(filename, "r")
-        output = ""
-        exists = False
-        # searches file to find line with matching key
         for line in file:
-            line = line[:-1] # strips newline from end of line
-            # line anatomy:
-            # key; numberOfPages; doc_url1, term_freq1; doc_url2, term_freq2
+            line = line[:-1]  # strips newline from end of line
             list = line.split("; ")
-            if list[0] == key:
-                exists = True
-                numberOfPages = list[1]
-                pages = list[2:]
-
-                #output += key + "; " + list[1]+1
-                #for page in pages:
-                #    output += "; " + page
-                output += key + "; " + str(int(numberOfPages)+1) + "; "
-                for page in pages:
-                    output += page + "; "
-                output += url + ", " + str(freq) + "\n"
-            else:
-                output += line + "\n"
-        file.close()
-        # key does not exist in file
-        if exists == False:
-            file = open(filename, "w")
-            output += key + "; 1; " + url + ", " + str(freq) + "\n"
-            file.write(output)
-        # rewrites the file with updated information
-        else:
-            file = open(filename, "w")
-            file.write(output)
-    # if file doesn't exist, create it with 1 entry
+            postings = list[1:]
+            dic.update({list[0]: list[1:]})
+    # if file doesn't exist create it
     except IOError:
         file = open(filename, "w")
-        out = key + "; 1; " + url + ", " + str(freq) + "\n"
-        file.write(out)
+        file.close()
+
+    # populate dic with given arrays
+    for key, url, freq in zip(keys, urls, freqs):
+        postings = dic.get(key, 500)
+        if postings == 500:
+            arr = []
+            arr.append(str(url+", "+str(freq)))
+            dic.update({key: arr})
+        else:
+            postings.append(str(url+", "+str(freq)))
+            dic.update({key: postings})
+
+    # write dic to file
+    file = open(filename, "w")
+    for token in dic:
+        postings = ""
+        try:
+            postings = "; ".join(dic.get(token))
+        except TypeError:
+            postings
+        file.write(token+"; "+postings+"\n")
     file.close()
 
+    # update index_info file
     indexInfo = open("index_info.txt", "w")
-    totalPageNum = int(totalPageNum) + 1
+    totalPageNum = int(totalPageNum) + len(keys)
     indexInfo.write(str(totalPageNum))
     indexInfo.close()
+
+
 
 # returns the total number of pages indexed
 def get_index_total():
@@ -106,39 +102,31 @@ def get_pages(token):
     for line in file:
         line = line[:-1]
         # line anatomy:
-        # key; numberOfPages; doc_url1, term_freq1; doc_url2, term_freq2
+        # key; doc_url1, term_freq1; doc_url2, term_freq2
         list = line.split("; ")
         if list[0] == token:
             file.close()
-            return list[2:]
+            return list[1:]
     file.close()
     return False
 
 if __name__ == "__main__":
     x = get_index_total()
     print(x)
-    addPage_index_file("test", "url1", 10)
-    x = get_index_total()
-    print(x)
-    addPage_index_file("zebra", "url3", 10)
-    x = get_index_total()
-    print(x)
-    x = get_pages("test")
-    print(x)
-    x = get_pages("zebra")
-    print(x)
-    addPage_index_file("test", "url2", 5)
+
+    addPage_index_file(["test", "zebra"], ["url1","url2"], [10,2])
     x = get_index_total()
     print(x)
     x = get_pages("test")
     print(x)
     x = get_pages("zebra")
     print(x)
-    addPage_index_file("123", "url2", 2)
-    x = get_index_total()
-    print(x)
-    addPage_index_file("abc", "url2", 15)
+
+
+    addPage_index_file(["test", "zebra"], ["url3","url4"], [5,6])
     x = get_index_total()
     print(x)
     x = get_pages("test")
+    print(x)
+    x = get_pages("zebra")
     print(x)
